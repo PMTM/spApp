c384c58ef61530a6b15a1695e69a0aabe4d802be
Petr Moravek <petr.moravek@t-mobile.cz>
2013-05-28 16:54:52 +0200

    added 3 buttons to cover 4 use cases

--- src/cz/xlinux/spApp/SPActivity.java
+++ src/cz/xlinux/spApp/SPActivity.java
@@ -1,7 +1,15 @@
 package cz.xlinux.spApp;
 
-import cz.xlinux.libAPI.aidl.EntryPoint;
-import cz.xlinux.libAPI.aidl.SecurityWatchdog;
+import aidl.core.API.EntryPoint;
+import aidl.core.API.OnCouponChange;
+import aidl.core.API.OnNewHistoryItem;
+import aidl.core.API.OnNewReceipt;
+import aidl.core.API.OnTicketChange;
+import aidl.core.API.SecurityWatchdog;
+import aidl.sp.API.Coupon;
+import aidl.sp.API.HistoryItem;
+import aidl.sp.API.Receipt;
+import aidl.sp.API.Ticket;
 import cz.xlinux.libAPI.libFce.APIConnection;
 import cz.xlinux.libAPI.libFce.CBOnSvcChange;
 import cz.xlinux.spApp.R;
@@ -25,6 +33,15 @@ public class SPActivity extends Activity implements OnClickListener,
 	private TextView mTvLog;
 	private boolean isBound;
 	private EntryPoint apiService;
+	private SecurityWatchdog wd;
+	private OnCouponChange couponCB;
+	private OnNewHistoryItem histCB;
+	private Coupon exCoupon;
+	private HistoryItem exHistoryItem;
+	private Ticket exTicket;
+	private Receipt exReceipt;
+	private OnTicketChange ticketCB;
+	private OnNewReceipt receiptCB;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -37,11 +54,21 @@ public class SPActivity extends Activity implements OnClickListener,
 		// mTvLog.setMovementMethod(ScrollingMovementMethod.getInstance());
 
 		Button clr;
-		clr = (Button) findViewById(R.id.btTestCert);
-		clr.setOnClickListener(this);
 		clr = (Button) findViewById(R.id.btGetWatchDog);
 		clr.setOnClickListener(this);
-		clr = (Button) findViewById(R.id.btTestScan);
+
+		clr = (Button) findViewById(R.id.btPushHist);
+		clr.setOnClickListener(this);
+		clr = (Button) findViewById(R.id.btPushCoupon);
+		clr.setOnClickListener(this);
+		clr = (Button) findViewById(R.id.btPullCoupon);
+		clr.setOnClickListener(this);
+
+		clr = (Button) findViewById(R.id.btPushReceipt);
+		clr.setOnClickListener(this);
+		clr = (Button) findViewById(R.id.btPushTicket);
+		clr.setOnClickListener(this);
+		clr = (Button) findViewById(R.id.btPullTicket);
 		clr.setOnClickListener(this);
 		// ---
 
@@ -63,18 +90,143 @@ public class SPActivity extends Activity implements OnClickListener,
 		case R.id.btGetWatchDog:
 			getWatchDog();
 			break;
+		case R.id.btPushHist:
+			pushHistory();
+			break;
+		case R.id.btPushCoupon:
+			pushCoupon();
+			break;
+		case R.id.btPullCoupon:
+			pullCoupon();
+			break;
+		case R.id.btPushReceipt:
+			pushReceipt();
+			break;
+		case R.id.btPushTicket:
+			pushTicket();
+			break;
+		case R.id.btPullTicket:
+			pullTicket();
+			break;
+		}
+	}
+
+	private void initCoupon() {
+		if (exCoupon == null) {
+			exCoupon = new Coupon("Coupon", "DM Drogerie");
+		}
+	}
+
+	private void initTicket() {
+		if (exTicket == null) {
+			exTicket = new Ticket("Ticket", "U2 Strahov 22.5.2000");
+		}
+	}
+
+	private void initHistoryItem() {
+		if (exHistoryItem == null) {
+			exHistoryItem = new HistoryItem("Paid", "2.346USD");
+		}
+	}
+
+	private void initReceipt() {
+		if (exReceipt == null) {
+			exReceipt = new Receipt("Paid", "2.346USD");
+		}
+	}
+
+	private void pullCoupon() {
+		initCoupon();
+		if (couponCB != null) {
+			try {
+				couponCB.removeCoupon(exCoupon);
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			Log.e(LOG_TAG, "no couponCB:-(");
+		}
+	}
+
+	private void pushCoupon() {
+		initCoupon();
+		if (couponCB != null) {
+			try {
+				couponCB.addCoupon(exCoupon);
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			Log.e(LOG_TAG, "no couponCB:-(");
+		}
+	}
+
+	private void pullTicket() {
+		initTicket();
+		if (ticketCB != null) {
+			try {
+				ticketCB.removeTicket(exTicket);
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			Log.e(LOG_TAG, "no TicketCB:-(");
+		}
+	}
+
+	private void pushTicket() {
+		initTicket();
+		if (ticketCB != null) {
+			try {
+				ticketCB.addTicket(exTicket);
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			Log.e(LOG_TAG, "no TicketCB:-(");
+		}
+	}
+
+	private void pushHistory() {
+		initHistoryItem();
+		if (histCB != null) {
+			try {
+				histCB.addHistoryItem(exHistoryItem);
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			Log.e(LOG_TAG, "no histCB:-(");
+		}
+	}
+	
+	private void pushReceipt() {
+		initReceipt();
+		if (receiptCB != null) {
+			try {
+				receiptCB.addReceipt(exReceipt);
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			Log.e(LOG_TAG, "no receiptCB:-(");
 		}
 	}
 
 	private void getWatchDog() {
 		if (apiService != null) {
 			try {
-				SecurityWatchdog wd = apiService.getSecurityWatchdog();
 				if (wd != null) {
 					wd.renewTimer();
 					wd.expireTimerNow();
 				} else {
-					Log.d(LOG_TAG,"Watchdog was not delivered");
+					Log.d(LOG_TAG, "Watchdog was not delivered");
 				}
 			} catch (RemoteException e) {
 				// TODO Auto-generated catch block
@@ -99,5 +251,17 @@ public class SPActivity extends Activity implements OnClickListener,
 	public void setService(EntryPoint apiService) {
 		Log.d(LOG_TAG, "setService apiService= " + apiService);
 		this.apiService = apiService;
+		if (apiService!=null) {
+			try {
+				wd = apiService.getSecurityWatchdog();
+				couponCB = apiService.getCouponCB();
+				histCB = apiService.getHistoryCB();
+				ticketCB = apiService.getTicketCB();
+				receiptCB = apiService.getReceiptCB();
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
